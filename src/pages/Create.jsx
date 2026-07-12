b8c80cb3e4eb6b416bf072e4d4129aec045dd8fd
manas022005 <manasbhatt022005@gmail.com>
2026-07-12 20:38:02 +0530

    changes on create section not properly rendering need to fix that

--- src/pages/Create.jsx
+++ src/pages/Create.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import { useForm } from 'react-hook-form'
 
 
 const Create = () => {
+  const {register, handleSubmit }=useForm();
   return (
     <div>
-      <form action=" "></form>
+      <form> 
+        <input className='block border-b outline-0 p-2' 
+        {...register ('image')} type="file" />hi
+        <input className='block border-b outline-0 p-2' 
+        {...register ('title')} type="text" placeholder='Recipe Title'/>
+      
+      </form>
     </div>
   )
 }
